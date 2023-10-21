1cb1a0bbe2d3982086b65186eaf415bebf534700
felixng <ngthinh302@gmail.com>
2023-10-21 01:10:43 -0400

    feat: Implement auth with Clerk

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ClerkProvider } from "@clerk/clerk-react";
+import { CLERK_PUBLISHABLE_KEY } from "./config";
+import { BrowserRouter, useNavigate } from "react-router-dom";
+import { Routes } from "./Routes";
+import { CssBaseline } from "@mui/material";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+if (!CLERK_PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
 }
 
-export default App
+const AuthProviderWithRoutes = () => {
+  const navigate = useNavigate();
+
+  return (
+    <ClerkProvider
+      publishableKey={CLERK_PUBLISHABLE_KEY}
+      navigate={(to) => navigate(to)}
+    >
+      <Routes />
+    </ClerkProvider>
+  );
+};
+
+function App() {
+  return (
+    <BrowserRouter>
+      <CssBaseline />
+      <AuthProviderWithRoutes />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/Routes.tsx
@@ -0,0 +1,56 @@
+import {
+  SignIn,
+  SignedIn,
+  SignUp,
+  SignedOut,
+  RedirectToSignIn,
+  UserButton,
+} from "@clerk/clerk-react";
+import { Route, Routes as RouterRoutes } from "react-router-dom";
+
+function PublicPage() {
+  return (
+    <>
+      <h1>Public page</h1>
+      <a href="/protected">Go to protected page</a>
+    </>
+  );
+}
+
+function ProtectedPage() {
+  return (
+    <>
+      <h1>Protected page</h1>
+      <UserButton />
+    </>
+  );
+}
+
+export const Routes = () => {
+  return (
+    <RouterRoutes>
+      <Route path="/" element={<PublicPage />} />
+      <Route
+        path="/sign-in/*"
+        element={<SignIn routing="path" path="/sign-in" />}
+      />
+      <Route
+        path="/sign-up/*"
+        element={<SignUp routing="path" path="/sign-up" />}
+      />
+      <Route
+        path="/protected"
+        element={
+          <>
+            <SignedIn>
+              <ProtectedPage />
+            </SignedIn>
+            <SignedOut>
+              <RedirectToSignIn />
+            </SignedOut>
+          </>
+        }
+      />
+    </RouterRoutes>
+  );
+};
