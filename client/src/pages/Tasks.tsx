d27a9eba2cd7bfbb061320277fae061ef9d10c39
crackalamoo <harysdalvi@gmail.com>
2023-10-21 14:31:46 -0400

    tasks page basic interface

--- client/src/pages/Tasks.tsx
+++ client/src/pages/Tasks.tsx
@@ -1,7 +1,123 @@
+import { useState, useEffect } from 'react';
+import { fetchEndpoint } from '@/utils/fetch';
+
+type Task = {
+  name: string;
+  desc: string;
+  emoji: string;
+  completed: boolean;
+  pinned: boolean;
+  deadline: number;
+  completed_at: number;
+}
+
 export const Tasks = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  useEffect(() => {
+    fetchEndpoint('reminders?id=0', 'GET').then((data) => {
+      setTasks(data.sort((a:Task,b:Task) => {
+        if (a.deadline > b.deadline) {
+          return -1;
+        } else if (a.deadline < b.deadline) {
+          return 1;
+        } else {
+          return 0;
+        }
+      }));
+      console.log(data);
+    });
+  }, []);
+
+  const formatDate = (d: number, inFuture=true) => {
+    const roundDay = (n: number) => Math.floor((new Date(n).getTime())/86400000);
+
+    if (inFuture && (roundDay(d*1000) - roundDay(Date.now()) === 0)) {
+      return 'today ' + (new Date(d*1000).toLocaleTimeString('en-US', {
+        hour: 'numeric',
+        minute: 'numeric',
+      }));
+    } else if (inFuture && (roundDay(d*1000) - roundDay(Date.now()) === 1)) {
+      return 'tomorrow ' + (new Date(d*1000).toLocaleTimeString('en-US', {
+        hour: 'numeric',
+        minute: 'numeric',
+      }));
+    }
+
+    let f = new Date(d*1000).toLocaleDateString('en-US', {
+              weekday: ((roundDay(d*1000) - roundDay(Date.now()))*(inFuture ? 1 : -1) > 7) ? 'short' : 'long',
+              year: (new Date(d*1000).getFullYear() === new Date().getFullYear()) ? undefined : 'numeric',
+              month: ((roundDay(d*1000) - roundDay(Date.now()))*(inFuture ? 1 : -1) > 7) ? 'long' : undefined,
+              day: ((roundDay(d*1000) - roundDay(Date.now()))*(inFuture ? 1 : -1) > 7) ? 'numeric' : undefined,
+              hour: inFuture ? 'numeric' : undefined,
+              minute: inFuture ? 'numeric' : undefined,
+            });
+    if (!inFuture && (roundDay(d*1000) - roundDay(Date.now()))*(-1) <= 7) {
+      if (roundDay(d*1000) - roundDay(Date.now()) === 0) {
+        return 'today';
+      }
+      if (roundDay(d*1000) - roundDay(Date.now()) === -1) {
+        return 'yesterday';
+      }
+      f = 'last '+f;
+    }
+    return f;
+  }
+
   return (
     <>
       <h1>Tasks</h1>
+
+      <h2>To-Do</h2>
+      <ul>
+        {tasks.map((task) => (
+          task.completed || !task.pinned || Date.now() > task.deadline*1000 ? <></>
+          :
+          <li>
+            📌{task.emoji} {task.name}
+            {task.desc ? <><br/>{task.desc}</> : <></>}
+            <br/>Do by {formatDate(task.deadline)}
+          </li>
+        ))}
+      </ul>
+
+      <ul>
+        {tasks.map((task) => (
+          task.completed || task.pinned || Date.now() > task.deadline*1000 ? <></>
+          :
+          <li>
+            {task.emoji} {task.name}
+            {task.desc ? <><br/>{task.desc}</> : <></>}
+            <br/>Do by {formatDate(task.deadline)}
+          </li>
+        ))}
+      </ul>
+
+      <h2>Completed</h2>
+      <ul>
+        {tasks.map((task) => (
+          task.completed ? 
+          <li>
+            {task.emoji} {task.name}
+            {task.desc ? <><br/>{task.desc}</> : <></>}
+            <br/>Finished {formatDate(task.completed_at, false)}
+          </li>
+          :
+          <></>
+        ))}
+      </ul>
+
+      <h2>Missed</h2>
+      <ul>
+        {tasks.map((task) => (
+          task.completed || Date.now() < task.deadline*1000 ? <></>
+          :
+          <li>
+            {task.emoji} {task.name}
+            {task.desc ? <><br/>{task.desc}</> : <></>}
+            <br/>Supposed to do by {formatDate(task.deadline, false)}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
--- server/services/seed.py
+++ server/services/seed.py
@@ -49,7 +49,7 @@ def seed_db():
         'emoji': '👨‍💻',
         'category': 'Work',
         'owner_id': 0,
-        'deadline': 1697806983+2*86400,
+        'deadline': 1697806983+4*86400,
         'pinned': False,
         'habit_frequency': 0,
         'incentive_min': 2,
@@ -66,7 +66,7 @@ def seed_db():
         'emoji': '🏋️',
         'category': 'Health/Fitness',
         'owner_id': 0,
-        'deadline': 1697808031+2*86400,
+        'deadline': 1697808031+3*86400,
         'pinned': False,
         'habit_frequency': 2,
         'incentive_min': 2,
@@ -111,14 +111,17 @@ def seed_db():
         'created_at': 1695302602
     })
 
-    for i in range(20):
+    for i in range(10):
+        task_type = random.randint(0, 4)
+        names = ['Take out the trash', 'Do laundry', 'Wash dishes', 'Water plants', 'Read a chapter of a book']
+        emojis = ['🗑️', '🧺', '🍽️', '🪴', '📚']
         reminder.create_reminder({
-            'name': 'Take out the trash',
+            'name': names[task_type],
             'desc': '',
-            'emoji': '🗑️',
+            'emoji': emojis[task_type],
             'category': 'Home',
             'owner_id': 0,
-            'deadline': 1695302602 + 86400*5 - i*86400*5,
+            'deadline': 1695302602 + 86400 - i*86400*10,
             'pinned': False,
             'habit_frequency': 0,
             'incentive_min': 0.1,
@@ -127,11 +130,11 @@ def seed_db():
         })
         # reminder.check_reminder(i+5)
         reminder.update_reminder(i+5, {
-            'created_at': 1695302602 - i*86400*5
+            'created_at': 1695302602 - i*86400*10
         })
         if random.random() > 0.1:
             reminder.update_reminder(i+5, {
-                'completed_at': 1695302602 + 86400*5 - i*86400*5 - 3600,
+                'completed_at': 1695302602 + 86400*10 - i*86400*10 - 3600,
                 'completed': int(True)
             })
     
@@ -141,16 +144,34 @@ def seed_db():
         'emoji': '👨‍💻',
         'category': 'Work',
         'owner_id': 0,
-        'deadline': 1697808031,
+        'deadline': 1697832000,
         'pinned': False,
         'habit_frequency': 0,
         'incentive_min': 1,
         'incentive_max': 3,
         'org_id': 0
-    })['id'] == 25
-    reminder.update_reminder(25, {
-        'created_at': 1697808031 - 86400
+    })['id'] == 15
+    reminder.check_reminder(15)
+    reminder.update_reminder(15, {
+        'created_at': 1697808031 - 86400,
+        'completed_at': 1697831000
     })
-    reminder.check_reminder(25)
+
+    reminder.create_reminder({
+        'name': 'Win HackHarvard',
+        'desc': 'In it to win it!',
+        'emoji': '💻',
+        'category': 'Work',
+        'owner_id': 0,
+        'deadline': 1697976000,
+        'pinned': True,
+        'habit_frequency': 0,
+        'incentive_min': 2,
+        'incentive_max': 5.7,
+        'friend_id': 1
+    })['id'] == 1
+    reminder.update_reminder(1, {
+        'created_at': 1697406983
+    })
     
     print(user.get_user(0))
