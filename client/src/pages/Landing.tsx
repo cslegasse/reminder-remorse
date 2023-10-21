3e738971f73f586cb801ca52ef61e182b479969c
cslegasse <legasseahs1@gmail.com>
2023-10-21 18:34:42 -0400

    added very good front end

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,7 +1,15 @@
+import logo from "../../public/screenshots/Reminders_Remorse_logo.png"
+
+
 export const Landing = () => {
+  const text = "Reminder apps are bland and don't keep you on track or help you build consistent habits. Hold yourself and your friends accountable. You’ll be remorseful when it hits your bank account otherwise.";
+
   return (
-    <>
-      <h1>Landing page</h1>
-    </>
+    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh", textAlign: "center", marginTop: "-135px" }}>
+      <img src={logo} alt="Reminders Remorse Logo" style={{ width: "50%" }} />
+      {text.split('.').map((sentence, index) => (
+        <p key={index} style={{ textAlign: "center", marginBottom: "10px" }}>{sentence.trim()}</p>
+      ))}
+    </div>
   );
 };
