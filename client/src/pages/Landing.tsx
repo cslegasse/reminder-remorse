395918b7637a25fc2ea0cdc2aa112f8665dd2171
crackalamoo <harysdalvi@gmail.com>
2023-10-22 00:39:41 -0400

    basic landing page

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,7 +1,7 @@
 import logo from "@/assets/RR_curve.png";
 
 export const Landing = () => {
-  //const text = "We were frustrated with the normal reminders app we all have on our phones. They are limited in their usefulness and are not very motivating. With goals being a big part of our lives, we wanted an app that could be so much more alongside our friends. We designed Reminder's Remorse to give people motivation to become consistent goal setters and create positive habits for themselves. There are four useful components that make Reminder's Remorse better: habit builder, reminder penalties, charity search, and friend exploration."
+  // const text = "We were frustrated with the normal reminders app we all have on our phones. They are limited in their usefulness and are not very motivating. With goals being a big part of our lives, we wanted an app that could be so much more alongside our friends. We designed Reminder's Remorse to give people motivation to become consistent goal setters and create positive habits for themselves. There are four useful components that make Reminder's Remorse better: habit builder, reminder penalties, charity search, and friend exploration."
   return (
     <div
       style={{
@@ -13,9 +13,55 @@ export const Landing = () => {
         marginTop: "-425px",
       }}
     >
-      <div style={{ float: "left", marginRight: "-500px" }}>
+      <div style={{ margin: 'auto', textAlign: 'center', marginTop: '1500px' }}>
         <img src={logo} alt="Reminders Remorse Logo" style={{ width: "40%" }} />
       </div>
+
+      <div>
+      <p style={{textAlign: 'center', fontSize: '200%'}}>
+      Reminder apps are bland and don't keep you on track or help you build consistent habits. Hold yourself and your friends accountable.<br/>
+      <b>Otherwise, you’ll be remorseful when it hits your bank account.</b>
+      </p>
+      <p>
+      Reminder's Remorse charges you when you miss a reminder and sends the money to a charity or a friend. Reminder's Remorse not only keeps track of your reminders, it shows your consistent habits, your all time total tasks, and penalizes you by sending money to a friend or charity when you don't complete a task on time. This app is designed to help you stay on top of your tasks and help others in the process. 
+      </p>
+
+      <h2>Inspiration</h2>
+      <p>
+      We were frustrated with the normal reminders app we all have on our phones. They are limited in their usefulness and are not very motivating. With goals a big part of our lives, we wanted an app that could be so much more alongside our friends. We designed Reminder's Remorse to give people motivation to become consistent goal setters and create positive habits for themselves. There are four useful components that make Reminder's Remorse better: habit builder, reminder penalties, charity search, and friend exploration.
+      </p>
+
+      <h2>Tech Stack</h2>
+      <p>
+      Frontend: React, Typescript<br/>
+      Backend: Flask, Redis, Python<br/>
+      Server: GoDaddy, Cloudflare Pages
+      </p>
+
+      <h2>Features</h2>
+      <h3>Reminder Penality</h3>
+      <p>
+      Users are charged a random amount of money when they miss a reminder. The money is sent to a charity or a friend.
+      </p>
+      <h3>Peer Pressure</h3>
+      <p>
+      Users can add friends to their account. Friends can see other friends' public reminders and can send them bumps to remind them of their reminders.
+      </p>
+
+      <h3>Habit Builder</h3>
+      <p>
+      Users can set up recurring reminders to build habits.
+      </p>
+
+      <h3>Charity Search</h3>
+      <p>
+      Users can search for charities to send their money to.
+      </p>
+
+      <h3>Personal Tracker</h3>
+      <p>Users can see reminders, habits, penalties, and charity donations in a dashboard.</p>
+      </div>
+      <div><br/><br/><br/><br/><br/><br/>&nbsp;</div>
     </div>
   );
 };
